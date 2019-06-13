920920f72e6ce5eb93ec4287f208fbaac9c8831a
vatican1 <vatican2001@gmail.com>
2019-06-13 18:20:59 +0300

    Fix of absence of buttons

--- app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/MainActivity.kt
+++ app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/MainActivity.kt
@@ -4,25 +4,30 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var mediaPlayer: MediaPlayer? =null
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MY_DEBUG","onCreate active")
+
+
         var mediaPlayer = MediaPlayer.create(applicationContext,R.raw.ne_s_nachala_oxxymiron )
         startMusic.setOnClickListener{
-            mediaPlayer.start()
+            mediaPlayer!!.start()
         }
-
         pause.setOnClickListener {
             mediaPlayer!!.stop()
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("MY_DEBUG","onStart active")
     }
 
     override fun onPause() {
