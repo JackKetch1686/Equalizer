ba6ec3d640df433718631f5055f45ab468dcb332
Viktor <vatican2001@gmail.com>
2019-06-13 15:55:51 +0300

    added permissions for writing and reading files

--- app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/MainActivity.kt
+++ app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/MainActivity.kt
@@ -1,12 +1,35 @@
 package ru.spb.designedBy239School.advancedMusicPlayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? =null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+        var mediaPlayer = MediaPlayer.create(applicationContext,R.raw.ne_s_nachala_oxxymiron )
+        startMusic.setOnClickListener{
+            mediaPlayer.start()
+        }
+
+        pause.setOnClickListener {
+            mediaPlayer!!.stop()
+        }
+
     }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer!!.stop()
+        mediaPlayer!!.release()
+
+    }
+
 }
