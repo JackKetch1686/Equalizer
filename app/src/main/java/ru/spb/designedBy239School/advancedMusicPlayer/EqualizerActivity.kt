8b808c179eb420296527d76c728ea0eac113c921
JackKetch1686 <senior.kotovaleksandr@yandex.ru>
2019-06-17 12:38:36 +0300

    IT FUCKING WORK!!! First working Equalizer

--- app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/EqualizerActivity.kt
+++ app/src/main/java/ru/spb/designedBy239School/advancedMusicPlayer/EqualizerActivity.kt
@@ -1,5 +1,6 @@
 package ru.spb.designedBy239School.advancedMusicPlayer
 
+import android.media.audiofx.BassBoost
 import android.media.audiofx.Equalizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,49 +15,88 @@ class EqualizerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equalizer)
 
-        if(intent.hasExtra("Session_Id")) {
+        if (intent.hasExtra("Session_Id")) {
             Log.d("EQ", "has Extra " + intent.getStringExtra("Session_Id"))
             text_view.text = intent.getCharSequenceExtra("Session_Id")
         }
 
+        val bassboost = BassBoost(0, intent.getStringExtra("Session_Id").toInt())
+
         val equalizer = Equalizer(0, intent.getStringExtra("Session_Id").toInt())
         equalizer.enabled = true
 
-        if (equalizer.enabled || equalizer.hasControl()) Log.d("EQ",
-            "equalizer is enable and has control")
+        if (equalizer.enabled && equalizer.hasControl()) Log.d(
+            "EQ",
+            "equalizer is enable and has control"
+        )
 
         Log.d("EQ", "Start level is ${equalizer.getBandLevel(0)}")
+        Log.d(
+            "EQ", "lowest level is ${equalizer.bandLevelRange[0]} " +
+                    "and highest is ${equalizer.bandLevelRange[1]}"
+        )
 
         text_preset.text = equalizer.getPresetName(equalizer.currentPreset)
         text_number_of_bands.text = equalizer.numberOfBands.toString()
         tex_min_freq.text = equalizer.getBandFreqRange(0)[0].toString()
         tex_min_freq.text = equalizer.getBandFreqRange(0)[1].toString()
 
+        eq_seek_bar_0.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                equalizer.setBandLevel(0, seekBar.progress.toShort())
+                Log.d("EQ", "BAnd level is ${equalizer.getBandLevel(0)}")
+            }
+        })
 
-        val bars = arrayOf(eq_seek_bar_0, eq_seek_bar_1, eq_seek_bar_2, eq_seek_bar_3, eq_seek_bar_4)
-        for(bar in bars) {
-            bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                }
+        eq_seek_bar_1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                equalizer.setBandLevel(1, seekBar.progress.toShort())
+                Log.d("EQ", "BAnd level is ${equalizer.getBandLevel(0)}")
+            }
+        })
 
-                override fun onStartTrackingTouch(p0: SeekBar?) {
-                }
+        eq_seek_bar_2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                equalizer.setBandLevel(2, seekBar.progress.toShort())
+                Log.d("EQ", "BAnd level is ${equalizer.getBandLevel(2)}")
+            }
+        })
 
-                override fun onStopTrackingTouch(seekBar : SeekBar?) {
-                    val bandNumber = bar.id.toString()[bar.id.toString().length-1].toShort()
-                    equalizer.setBandLevel(bandNumber, seekBar!!.progress.toShort())
-                    text_band_0.text = equalizer.getBandLevel(bandNumber).toString()
-                }
-            })
-        }
-//        val bands = Array<Short>(5) { i->i.toShort() }
-//        for (band in bands) {
-//            Log.d("EQ",("${equalizer.getBandFreqRange(band)[0]} ${equalizer.getBandFreqRange(band)[1]}"))
-//        }
+        eq_seek_bar_3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                equalizer.setBandLevel(3, seekBar.progress.toShort())
+                Log.d("EQ", "BAnd level is ${equalizer.getBandLevel(3)}")
+            }
+        })
+
+        eq_seek_bar_4.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                equalizer.setBandLevel(4, seekBar.progress.toShort())
+                Log.d("EQ", "BAnd level is ${equalizer.getBandLevel(4)}")
+            }
+        })
+
+
     }
 
-    override fun onStop() {
-        super.onStop()
-
-    }
 }
